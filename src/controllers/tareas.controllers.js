66a4c6a2fe131b739db067b0939ba3a6fbf536ef
Mauricio Soria <mauricioutn2017@gmail.com>
2023-07-06 14:09:28 -0300

    listo el metodo de borrado

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -23,6 +23,19 @@ export const obtenerTarea = async (req, res) => {
         });
     };
 }
+export const borrarTarea = async (req, res) => {
+    try {
+        const tareas = await Tarea.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            mensaje:'borrado exitoso'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: 'Error al borrar Tarea',
+        });
+    };
+}
 export const crearTarea = async (req, res) => {
     try {
         const nuevaTarea= new Tarea(req.body);
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,8 +1,8 @@
 import {Router} from'express';
-import { obtenerTareas, crearTarea ,obtenerTarea} from '../controllers/tareas.controllers';
+import { obtenerTareas, crearTarea ,obtenerTarea,borrarTarea} from '../controllers/tareas.controllers';
 
 const router= Router();
 
 router.route("/tareas").get(obtenerTareas).post(crearTarea);
-router.route("/tareas/:id").get(obtenerTarea);
+router.route("/tareas/:id").get(obtenerTarea).delete(borrarTarea);
 export default router;obtenerTarea
